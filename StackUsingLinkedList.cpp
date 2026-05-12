78c13b976bff8389dbfb6256b1950129b8f0c3fa
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-12 11:36:20 +0700

    menambahkan operasi pop dan peek pada menu

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -106,4 +106,14 @@ int main()
                 cout << "Stack is empty. Cannot pop." << endl;
             }
             break;
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();//get the value of the top element
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot peek." << endl;
+            }
+            break;
         }
